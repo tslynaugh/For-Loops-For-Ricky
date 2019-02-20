5fb2df77118904dd1398a0f3ebd440db6d23f13f
RadiantBytes <lynaugh.thomas@uwlax.edu>
2019-02-20 11:04:26 -0600

    First commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+/*
+ * For Loops For Ricky
+ * 
+ *  A quick program to help my good pal Ricky
+ *  With his for loops
+ *  
+ *  @author Thomas Lynaugh
+ *  
+ *  Last Modified: February 20, 2019
+ */
+public class Main {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i < 7; i++) {
+			for (int d = i; d < 7; d++) {
+
+				System.out.print(d * 10 + " ");
+			}
+			System.out.println();
+
+		}
+
+	}
+
+}
